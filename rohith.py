59d9a0a641b52a4356b38e2e1584494c66da5c46
RohithMukkamula <87118119+RohithMukkamula@users.noreply.github.com>
2024-03-13 16:02:34 +0530

    Update rohith.py

--- rohith.py
+++ rohith.py
@@ -4,9 +4,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import seaborn as sns
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 
 
 from wordcloud import WordCloud, STOPWORDS
