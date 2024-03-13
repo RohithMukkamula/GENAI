675cd46239b8659dfd3f9779b3fbc47481ee94b5
RohithMukkamula <87118119+RohithMukkamula@users.noreply.github.com>
2024-03-13 16:07:56 +0530

    Update rohith.py

--- rohith.py
+++ rohith.py
@@ -10,7 +10,10 @@ import numpy as np
 
 
 
-df = pd.read_csv("https://github.com/RohithMukkamula/GENAI/blob/main/concatenated_df1.csv")
+df = pd.read_csv("https://github.com/RohithMukkamula/GENAI/blob/main/concatenated_df1.csv",sep=',',  # Specify delimiter (adjust if needed)
+                   quotechar='"',  # Specify quote character (adjust if needed)
+                   skipinitialspace=True,  # Skip leading/trailing spaces
+                   error_bad_lines=False)
 
 # Sidebar selection
 year_selected = st.sidebar.selectbox("Select Year", [0] + df['year'].unique().tolist())
